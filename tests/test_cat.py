91a105eadd72ff9525329f7b9d332df13e635ad0
Yuyang Zhang <695441493@qq.com>
2016-12-26 13:21:33 +0800

    add more test

--- tests/test_cat.py
+++ tests/test_cat.py
@@ -2,34 +2,51 @@
 
 import os
 import shutil
+import unittest
 import datetime
+from peewee import IntegrityError
 from catstalk.cat import Cat
 from catstalk.static import POST_TEMPLATE
 from catstalk.models import *
 
 
-def test_generate():
-    path = "blog"
-    try:
-        Cat.generate(path)
-        assert os.path.exists(path)
-        assert os.path.exists(os.path.join(path, "content"))
-        assert os.path.exists(os.path.join(os.path.join(path, "content"), "HelloWorld.md"))
-    finally:
-        shutil.rmtree(path)
+class CatTestCase(unittest.TestCase):
+    def test_generate(self):
+        path = "blog"
+        try:
+            Cat.generate(path)
+            self.assertTrue(os.path.exists(path))
+            self.assertTrue(os.path.exists(os.path.join(path, "content")))
+            self.assertTrue(os.path.exists(os.path.join(os.path.join(path, "content"), "HelloWorld.md")))
+        finally:
+            shutil.rmtree(path)
 
-
-def test_compile_post():
-    # Without tag
-    try:
-        date = datetime.datetime.now()
-        str_date = date.strftime("%Y-%m-%d %H:%M:%S")
-        content = u"%s%s" % (
-            POST_TEMPLATE.replace("{{date}}", str_date).replace("{{title}}", "HelloWorld"), "Hello World!"
-        )
-        Cat.compile_post(content)
-        post = Post.select().where(Post.title == "HelloWorld").first()
-        assert post.date.strftime("%Y-%m-%d %H:%M:%S") == date.strftime("%Y-%m-%d %H:%M:%S")
-        # TODO: Add more tests
-    finally:
-        os.remove("blog_data.sqlite3")
+    def test_compile_post(self):
+        try:
+            # Without tag
+            date = datetime.datetime.now()
+            str_date = date.strftime("%Y-%m-%d %H:%M:%S")
+            content = u"%s%s" % (
+                POST_TEMPLATE.replace("{{date}}", str_date).replace("{{title}}", "HelloWorld"), "Hello World!"
+            )
+            Cat.compile_post(content)
+            post = Post.select().where(Post.title == "HelloWorld").first()
+            self.assertEqual(post.date.strftime("%Y-%m-%d %H:%M:%S"), date.strftime("%Y-%m-%d %H:%M:%S"))
+            self.assertEqual(post.content, "<p>Hello World!</p>\n")
+            # With tag
+            content = u"%s%s" % (
+                POST_TEMPLATE.replace("{{date}}", str_date).replace("{{title}}", "WithTag"), "Hello World!"
+            )
+            temp = content.split("---")
+            content = "---\n%s\ntag: test\n---%s" % (temp[1], temp[2])
+            Cat.compile_post(content)
+            tag = Tag.select().where(Tag.name == "test").first()
+            self.assertTrue(tag)
+            post = Post.select().where(Post.title == "WithTag").first()
+            self.assertEqual(post.date.strftime("%Y-%m-%d %H:%M:%S"), date.strftime("%Y-%m-%d %H:%M:%S"))
+            self.assertEqual(post.tag, tag)
+            self.assertEqual(post.content, "<p>Hello World!</p>\n")
+            # Unique
+            self.assertRaises(IntegrityError, Cat.compile_post, content)
+        finally:
+            os.remove("blog_data.sqlite3")
